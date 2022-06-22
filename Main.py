5fe5fc33643b0cbcdbc143e7e7149b40e8ed6e9a
HomHom1 <homahomen2@gmail.com>
2022-06-21 22:24:47 -0500

    Discord

--- Main.py
+++ Main.py
@@ -38,10 +38,13 @@ async def unload(ctx, extension):
 
 @client.command()
 async def reload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-    client.load_extension(f'cogs.{extension}')
-    await ctx.send('cog reloaded')
+    if ctx.message.author.guild_permissions.administrator:
 
+        client.unload_extension(f'cogs.{extension}')
+        client.load_extension(f'cogs.{extension}')
+        await ctx.send('cog reloaded')
+    else:
+        await ctx.send('You cannot run this command')
 
 
 #---------------------------------------------------------------ON GUILD JOIN/REMOVE ------------------------------------------------------------------------
